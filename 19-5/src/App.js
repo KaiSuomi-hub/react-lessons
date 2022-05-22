fb7b548116460e907c44ed804d3e25ed244c4c6c
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2022-05-22 11:39:33 +0300

    doesn't compile, but code is ok

--- 19-5/src/App.js
+++ 19-5/src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 import EulerPicture from "./assets/js/component/eulerpicture.component";
 import Nav from "./assets/js/component/navbar.component";
 // import { WikiSearch } from "./assets/js/component/wikisearch.component";
-// import { Singles } from "./assets/js/component/singles.component";
-// import { Input } from "./assets/js/component/input.component";
+import { Singles } from "./assets/js/component/singles.component";
+import { Input } from "./assets/js/component/input.component";
 function App() {
 
     //! input needed to be here, cannot import more after euler
@@ -22,12 +22,12 @@ function App() {
             </div>
             <div id="task2" className="shown ">
             <h1>This is task2</h1>
-
+            <Singles/>
 
             </div>
             <div id="task3">
                 <h1>This is task3</h1>
-
+                <Input/>
                 <i style={{marginTop:"-35px"}} >Font is at 2 rem</i>
 
                 <a target="_blank" href="https://codesandbox.io/s/angry-surf-r3bqd4?file=/src/App.js">Yup. See Link.</a>
